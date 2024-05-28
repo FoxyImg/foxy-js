8eb397b2b002d1bc285ae41120b2ee8ebb653c08
Jon Gilkison <jon.gilkison@gmail.com>
2024-05-28 13:57:57 +0700

    Fix signing

--- src/utils/sign.ts
+++ src/utils/sign.ts
@@ -1,6 +1,7 @@
 import { sha256 } from 'js-sha256';
 
 export default function signHMAC256(key: string, message: string) {
+	console.log(key, message);
 	const hash = sha256.hmac.create(key);
 	hash.update(message);
 	return hash.hex();
