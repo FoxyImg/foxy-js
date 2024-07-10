321fbf6b427a1a5bb44aae5f4373efbab2a28590
Jon Gilkison <jon.gilkison@gmail.com>
2024-07-11 00:49:06 +0700

    Added extras slot to modal container

--- packages/vue-ui/src/components/modals/ModalContainer.vue
+++ packages/vue-ui/src/components/modals/ModalContainer.vue
@@ -19,8 +19,11 @@ withDefaults(defineProps<{
 		<div class="bg-white rounded-lg flex flex-col w-full" :class="{'max-w-[min(90vw,480px)]': !fullScreen, 'min-w-[95vw] max-w-[95vw] min-h-[90vh] max-h-[90vh]': fullScreen }">
 			<div v-if="showTitle" class="flex items-center justify-between border-b border-b-neutral-300">
 				<div class="p-3 text-base font-bold">{{ title }}</div>
-				<div v-if="showClose" @click="emit('close')" class="p-3 cursor-pointer">
-					<CloseIcon class="fill-black w-2 h-auto" />
+				<div class="flex items-center">
+					<slot name="extras"></slot>
+					<div v-if="showClose" @click="emit('close')" class="p-3 cursor-pointer">
+						<CloseIcon class="fill-black w-2 h-auto" />
+					</div>
 				</div>
 			</div>
 			<slot></slot>
